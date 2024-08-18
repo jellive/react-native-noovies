3ec6f0106c77d92ec56a95988be93097a77d848c
Jell <mystral7@naver.com>
2024-08-19 03:14:07 +0900

    [Styled-components] add styled-components

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,12 +1,36 @@
-import react from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import styled from 'styled-components/native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+const Title = styled.Text`
+  color: ${props => (props.selected ? 'blue' : 'red')};
+`
+
+// const Header = styled.View``
+
+// const Column = styled.View``
+
+// const Footer = styled.View``
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate('Stack', { screen: 'Three' })} // 바로 'Three'를 쓰면 navigation 사이를 이동하기 때문에 안 됨.
-    style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <>
+    {/* <Header></Header>
+    <Column> */}
+    <Btn
+      onPress={() => navigate('Stack', { screen: 'Three' })} // 바로 'Three'를 쓰면 navigation 사이를 이동하기 때문에 안 됨.
+    >
+      <Title selected={true}>Movies</Title>
+      <Title selected={false}>Movies</Title>
+    </Btn>
+    {/* </Column>
+    <Footer></Footer> */}
+  </>
 )
+
 export default Movies
