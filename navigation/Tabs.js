7936f02a0ef7028ceb1a68a9d7eec152964447e0
Jell <mystral7@naver.com>
2024-08-19 01:43:28 +0900

    [UI] add tab bar icon

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,6 +5,7 @@ import Search from '../screens/Search'
 import { Text, View } from 'react-native'
 import { useColorScheme } from 'react-native'
 import { BLACK_COLOR, YELLOW_COLOR } from '../utils/colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
@@ -13,55 +14,72 @@ const Tabs = () => {
   console.log('isDark', isDark)
   return (
     <Tab.Navigator
-    /**
-     * @desc 초기 페이지 설정
-     */
-    // initialRouteName="Search"
-    /**
-     * @desc 탭바 배경 설정
-     */
-    // screenOptions={{ tabBarLabelStyle: { backgroundColor: 'red' } }}
-    /**
-     * @desc 아이콘 배치 설정
-     */
-    // screenOptions={{ tabBarLabelPosition: 'beside-icon' }}
-    /**
-     * @desc 탭바 색상 세부 설정
-     */
-    //   screenOptions={{
-    //     tabBarActiveTintColor: 'red',
-    //     tabBarInactiveTintColor: 'purple',
-    //     tabBarStyle: { backgroundColor: 'tomato' }
-    //   }}
-    /**
-     * @desc 다크모드 유무에 따른 탭바 색상 세부 설정
-     */
-    //   screenOptions={{
-    //     tabBarStyle: {
-    //       backgroundColor: isDark ? BLACK_COLOR : 'white'
-    //     },
-    //     tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
-    //     tabBarInactiveTintColor: isDark ? '#d2dae2' : '#808e9b',
-    //     headerStyle: {
-    //       backgroundColor: isDark ? BLACK_COLOR : 'white'
-    //     },
-    //     headerTitleStyle: {
-    //       color: isDark ? 'white' : BLACK_COLOR
-    //     }
-    //   }}
+      /**
+       * @desc 초기 페이지 설정
+       */
+      // initialRouteName="Search"
+      /**
+       * @desc 탭바 배경 설정
+       */
+      // screenOptions={{ tabBarLabelStyle: { backgroundColor: 'red' } }}
+      /**
+       * @desc 아이콘 배치 설정
+       */
+      // screenOptions={{ tabBarLabelPosition: 'beside-icon' }}
+      /**
+       * @desc 탭바 색상 세부 설정
+       */
+      //   screenOptions={{
+      //     tabBarActiveTintColor: 'red',
+      //     tabBarInactiveTintColor: 'purple',
+      //     tabBarStyle: { backgroundColor: 'tomato' }
+      //   }}
+      /**
+       * @desc 다크모드 유무에 따른 탭바 색상 세부 설정
+       */
+      //   screenOptions={{
+      //     tabBarStyle: {
+      //       backgroundColor: isDark ? BLACK_COLOR : 'white'
+      //     },
+      //     tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+      //     tabBarInactiveTintColor: isDark ? '#d2dae2' : '#808e9b',
+      //     headerStyle: {
+      //       backgroundColor: isDark ? BLACK_COLOR : 'white'
+      //     },
+      //     headerTitleStyle: {
+      //       color: isDark ? 'white' : BLACK_COLOR
+      //     }
+      //   }}
+      screenOptions={{
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 10,
+          fontWeight: 600
+        }
+      }}
     >
       <Tab.Screen
         name="Movies"
         component={Movies}
         options={{
-          headerTitleStyle: {
-            color: 'tomato'
-          },
-          headerRight: () => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          )
+          //   headerTitleStyle: {
+          //     color: 'tomato'
+          //   },
+          //   headerRight: () => (
+          //     <View>
+          //       <Text>Hello</Text>
+          //     </View>
+          //   ),
+          tabBarIcon: ({ focused, color, size }) => {
+            console.log(focused, color, size) // focused는 react navigator에서 처리해주기 떄문에 거의 쓸 일이 없음
+            return (
+              <Ionicons
+                name={focused ? 'film' : 'film-outline'}
+                color={color}
+                size={size}
+              />
+            )
+          }
         }}
       />
       <Tab.Screen
@@ -69,8 +87,29 @@ const Tabs = () => {
         component={Tv}
         //   options={{ tabBarLabelStyle: { backgroundColor: 'purple' } }}
         //   options={{ tabBarBadge: 'hello' }}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            console.log(color, size)
+            return <Ionicons name="tv-outline" color={color} size={size} />
+          }
+        }}
       />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            console.log(color, size)
+            return (
+              <Ionicons
+                name={focused ? 'search' : 'search-outline'}
+                color={color}
+                size={size}
+              />
+            )
+          }
+        }}
+      />
     </Tab.Navigator>
   )
 }
