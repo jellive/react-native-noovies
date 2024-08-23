d821264134aff4b05c41974c77cdb86af32ae748
Jell <mystral7@naver.com>
2024-08-23 21:17:23 +0900

    [Detail] add YouTube

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React, { useEffect } from 'react'
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Linking } from 'react-native'
 import styled from 'styled-components/native'
 import { Movie, moviesApi, TV, tvApi } from '../api'
 import Poster from '../components/Poster'
@@ -8,6 +8,8 @@ import { makeImgPath } from '../utils'
 import { LinearGradient } from 'expo-linear-gradient'
 import { BLACK_COLOR } from '../utils/colors'
 import { useQuery } from 'react-query'
+import Loader from '../components/Loader'
+import { Ionicons } from '@expo/vector-icons'
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 
@@ -35,10 +37,25 @@ const Title = styled.Text`
   margin-left: 15px;
   font-weight: 500;
 `
+
+const Data = styled.View`
+  padding: 0 20px;
+`
+
 const Overview = styled.Text`
   color: ${props => props.theme.textColor};
-  margin-top: 20px;
-  padding: 0 20px;
+  margin: 20px 0;
+`
+
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
 `
 
 type RootStackParamList = {
@@ -51,27 +68,23 @@ const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params }
 }) => {
-  const { isLoading: moviesLoading, data: moviesData } = useQuery(
-    ['movies', params.id],
-    moviesApi.detail,
-    {
-      enabled: 'original_title' in params
-    }
-  )
-  const { isLoading: tvLoading, data: tvData } = useQuery(
-    ['tv', params.id],
-    tvApi.detail,
-    {
-      enabled: 'original_name' in params
-    }
+  const isMovie = 'original_title' in params
+  const { isLoading, data } = useQuery(
+    [isMovie ? 'movies' : 'tv', params.id],
+    isMovie ? moviesApi.detail : tvApi.detail
   )
   useEffect(() => {
     setOptions({
       title: 'original_title' in params ? 'Movie' : 'TV Show'
     })
   }, [])
-  console.log('movies', moviesData)
-  console.log('tv', tvData)
+  console.log('data', data)
+
+  const openYTLink = async (videoID: string) => {
+    const baseUrl = `http://m.youtube.com/watch?v=${videoID}`
+    await Linking.openURL(baseUrl)
+  }
+
   return (
     <Container>
       <Header>
@@ -92,7 +105,16 @@ const Detail: React.FC<DetailScreenProps> = ({
           </Title>
         </Column>
       </Header>
-      <Overview>{params.overview}</Overview>
+      <Data>
+        <Overview>{params.overview}</Overview>
+        {isLoading && <Loader />}
+        {data?.videos?.results?.map(video => (
+          <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+            <Ionicons name="logo-youtube" color="white" size={24} />
+            <BtnText>{video.name}</BtnText>
+          </VideoBtn>
+        ))}
+      </Data>
     </Container>
   )
 }
