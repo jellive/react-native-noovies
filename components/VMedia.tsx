922c680574735ea385622e44f86b4658d52ad379
Jell <mystral7@naver.com>
2024-08-23 18:30:18 +0900

    [Detail] add detail & connect detail view

--- components/VMedia.tsx
+++ components/VMedia.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components/native'
 import Poster from './Poster'
 import Votes from './Votes'
+import { useNavigation } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native'
 
 const Movie = styled.View`
   align-items: center;
@@ -24,15 +26,23 @@ const VMedia: React.FC<VMediaProps> = ({
   posterPath,
   originalTitle,
   voteAverage
-}) => (
-  <Movie>
-    <Poster path={posterPath} />
-    <Title>
-      {originalTitle.slice(0, 12)}
-      {originalTitle.length > 12 ? '...' : null}
-    </Title>
-    <Votes votes={voteAverage} />
-  </Movie>
-)
+}) => {
+  const navigation = useNavigation() // 전역으로 navigation을 얻을 수 있음. 이로 페이지 이동이 가능.
+  const goToDetail = () => {
+    navigation.navigate('Stack', { screen: 'Detail' })
+  }
+  return (
+    <TouchableOpacity onPress={goToDetail}>
+      <Movie>
+        <Poster path={posterPath} />
+        <Title>
+          {originalTitle.slice(0, 12)}
+          {originalTitle.length > 12 ? '...' : null}
+        </Title>
+        <Votes votes={voteAverage} />
+      </Movie>
+    </TouchableOpacity>
+  )
+}
 
 export default VMedia
