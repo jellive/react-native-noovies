04bd99bbda73e7a270510793864630b0d3e96bdb
Jell <mystral7@naver.com>
2024-08-23 19:20:35 +0900

    [Typed] add type props

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,3 +1,4 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 import styled from 'styled-components/native'
@@ -6,7 +7,13 @@ const Container = styled.ScrollView`
   background-color: ${props => props.theme.mainBgColor};
 `
 
-const Detail = ({
+type RootStackParamList = {
+  Detail: { originalTitle: string } // 여기 이름은 screen의 이름으로 맞춰야 함.
+}
+
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, 'Detail'> // 여기도.
+
+const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: {
     params: { originalTitle }
