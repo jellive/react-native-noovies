4562909b7d840ba5b82dbead683cf8f7378b98cb
Jell <mystral7@naver.com>
2024-08-23 18:15:26 +0900

    [Search] add result

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -3,6 +3,8 @@ import { View, Text } from 'react-native'
 import { useQuery } from 'react-query'
 import styled from 'styled-components/native'
 import { moviesApi, tvApi } from '../api'
+import Loader from '../components/Loader'
+import HList from '../components/HList'
 
 const Container = styled.ScrollView``
 
@@ -12,6 +14,7 @@ const SearchBar = styled.TextInput`
   border-radius: 15px;
   width: 90%;
   margin: 10px auto;
+  margin-bottom: 40px;
 `
 
 const Search = () => {
@@ -49,7 +52,11 @@ const Search = () => {
         onChangeText={onChangeText}
         onSubmitEditing={onSubmit}
       />
-      <Text>{JSON.stringify(moviesData)}</Text>
+      {moviesLoading || tvLoading ? <Loader /> : null}
+      {moviesData ? (
+        <HList title="Movie Results" data={moviesData.results} />
+      ) : null}
+      {tvData ? <HList title="TV Results" data={tvData.results} /> : null}
     </Container>
   )
 }
